3a1956b1d350b2b9232c51fc69fab8c64d0a4f79
maan81 <pranijman@gmail.com>
2016-02-03 08:47:37 +0545

    37 Layouts --- only the Breadcrumbs removed from main layout

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -81,6 +81,8 @@ class SiteController extends Controller
             return $this->goHome();
         }
 
+        $this->layout = 'loginLayout';
+
         $model = new LoginForm();
         if ($model->load(Yii::$app->request->post()) && $model->login()) {
             return $this->goBack();
